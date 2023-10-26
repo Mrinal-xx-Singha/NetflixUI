c21263dfcf2088c21aaeb3b483005b6bd4d6a44d
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2023-10-26 18:39:02 +0530

    Updating code base

--- src/request.js
+++ src/request.js
@@ -1,7 +1,7 @@
 const API_KEY ="5a80c63c252c40df6b12809195937c58";
 
 const request = {
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
